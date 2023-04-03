493dc408c7281cbe3a87ae1ec887f9e03c2b04c6
Zakariye Abdi <zak.abdi94@gmail.com>
2023-04-03 09:09:45 -0500

    created todo app via vue

--- src/App.vue
+++ src/App.vue
@@ -41,12 +41,12 @@ watch(
 );
 
 watch(name, (newVal) => {
-  localStorage.setItem("name, newVal");
+  localStorage.setItem("name", newVal);
 });
 
 onMounted(() => {
   name.value = localStorage.getItem("name") || "";
-  todos.value = Json.parse(localStorage.getItem("todos")) || [];
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
 });
 </script>
 
